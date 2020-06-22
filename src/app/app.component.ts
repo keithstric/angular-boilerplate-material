da0efc1f76258b1195d3e7234678409858f7caad
keithstrickland <keith.strickland@randstadusa.com>
2020-06-22 13:50:51 -0400

    * added some scss mixins and implemented them where needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,28 @@ import {LoadingService} from 'src/app/layout/services/loading/loading.service';
 import {PROJECT_NAME} from 'src/environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = PROJECT_NAME;
-  loading: boolean = false;
+	title = PROJECT_NAME;
+	loading: boolean = false;
 
-  constructor(
-    private _loading: LoadingService
-  ){ }
+	constructor(
+		private _loading: LoadingService
+	) {
+	}
 
-  ngOnInit() {
-    this.listenToLoading();
-  }
+	ngOnInit() {
+		this.listenToLoading();
+	}
 
-  listenToLoading() {
-    this._loading.loadingSub
-      .pipe(delay(0)) // This prevents a ExpressionChangedAfterItHasBeenCheckedError for subsequent requests
-      .subscribe((loading) => {
-        this.loading = loading;
-      });
-  }
+	listenToLoading() {
+		this._loading.loadingSub
+			.pipe(delay(0)) // This prevents a ExpressionChangedAfterItHasBeenCheckedError for subsequent requests
+			.subscribe((loading) => {
+				this.loading = loading;
+			});
+	}
 }
--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -9,88 +9,89 @@ import {UiService} from 'src/app/core/services/ui/ui.service';
 import {PROJECT_NAME} from 'src/environments/environment';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['../auth-shared-styles.scss']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['../auth-shared-styles.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  loginData: FormGroup;
-  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
-  password: FormControl = new FormControl('', [Validators.required]);
-  errorMsg: string;
-  projectName: string = PROJECT_NAME;
-  subscriptions: Subscription = new Subscription();
+	loginData: FormGroup;
+	email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+	password: FormControl = new FormControl('', [Validators.required]);
+	errorMsg: string;
+	projectName: string = PROJECT_NAME;
+	subscriptions: Subscription = new Subscription();
 
-  constructor(
-    private _formBuilder: FormBuilder,
-    private _error: ErrorService,
-    private _auth: AuthService,
-    private _router: Router,
-    private _ui: UiService
-  ) { }
+	constructor(
+		private _formBuilder: FormBuilder,
+		private _error: ErrorService,
+		private _auth: AuthService,
+		private _router: Router,
+		private _ui: UiService
+	) {
+	}
 
-  ngOnInit(): void {
-    // Subscribe to the error service to catch errors
-    this.subscriptions.add(this._error.errorEvent.subscribe((err: Error) => {
-      this.errorMsg = err.message;
-    }));
-    if (this._auth.isAuthenticated()) {
-      this._router.navigateByUrl('/');
-    }
-    this.buildFormGroup();
-  }
+	ngOnInit(): void {
+		// Subscribe to the error service to catch errors
+		this.subscriptions.add(this._error.errorEvent.subscribe((err: Error) => {
+			this.errorMsg = err.message;
+		}));
+		if (this._auth.isAuthenticated()) {
+			this._router.navigateByUrl('/');
+		}
+		this.buildFormGroup();
+	}
 
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
-  }
+	ngOnDestroy(): void {
+		this.subscriptions.unsubscribe();
+	}
 
-  /**
-   * Build the formGroup
-   */
-  private buildFormGroup() {
-    this.loginData = this._formBuilder.group({
-      email: this.email,
-      password: this.password
-    });
-  }
+	/**
+	 * Build the formGroup
+	 */
+	private buildFormGroup() {
+		this.loginData = this._formBuilder.group({
+			email: this.email,
+			password: this.password
+		});
+	}
 
-  /**
-   * Gets the validation message to show for each field
-   * @param {string} field
-   */
-  getFieldErrorMessage(field: string) {
-    if (field === 'email') {
-      if (this.email.hasError('required')) {
-        return 'You must provide an email address';
-      }else if (this.email.hasError('email')) {
-        return 'Please enter a valid email address (yourname@knowhere.com)';
-      }
-    }else if (field === 'password') {
-      if (this.password.hasError('required')) {
-        return 'You must provide a password';
-      }
-    }
-    return '';
-  }
+	/**
+	 * Gets the validation message to show for each field
+	 * @param {string} field
+	 */
+	getFieldErrorMessage(field: string) {
+		if (field === 'email') {
+			if (this.email.hasError('required')) {
+				return 'You must provide an email address';
+			} else if (this.email.hasError('email')) {
+				return 'Please enter a valid email address (yourname@knowhere.com)';
+			}
+		} else if (field === 'password') {
+			if (this.password.hasError('required')) {
+				return 'You must provide a password';
+			}
+		}
+		return '';
+	}
 
-  /**
-   * Perform the authentication
-   */
-  loginClick() {
-    this.subscriptions.add(this._auth.login(this.loginData.getRawValue())
-      .subscribe((user: User) => {
-        this._ui.notifyUserShowSnackbar(`Welcome to ${PROJECT_NAME}!`);
-        this._router.navigateByUrl('/auth/user');
-      }));
-  }
+	/**
+	 * Perform the authentication
+	 */
+	loginClick() {
+		this.subscriptions.add(this._auth.login(this.loginData.getRawValue())
+			.subscribe((user: User) => {
+				this._ui.notifyUserShowSnackbar(`Welcome to ${PROJECT_NAME}!`);
+				this._router.navigateByUrl('/auth/user');
+			}));
+	}
 
-  /**
-   * On keypress if the key is the enter key, perform the login
-   * @param evt {KeyboardEvent}
-   */
-  onKeypress(evt: KeyboardEvent) {
-    if (evt.key === 'Enter' && !this.loginData.invalid) {
-      this.loginClick();
-    }
-  }
+	/**
+	 * On keypress if the key is the enter key, perform the login
+	 * @param evt {KeyboardEvent}
+	 */
+	onKeypress(evt: KeyboardEvent) {
+		if (evt.key === 'Enter' && !this.loginData.invalid) {
+			this.loginClick();
+		}
+	}
 }
